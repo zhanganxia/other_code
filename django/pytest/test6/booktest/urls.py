e063270ef416513bff54148f87549d042c5ef338
kk <kk@kk.rhel.cc>
2017-12-18 22:19:34 +0800

    test class view

--- django/pytest/test6/booktest/urls.py
+++ django/pytest/test6/booktest/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
-from booktest import views
+# from booktest import views
+from booktest.views import MyView
 
 urlpatterns = [
-    url(r'^session_set/$',views.session_set),
-    url(r'^session_get/',views.session_get),
+    # url(r'^session_set/$',views.session_set),
+    # url(r'^session_get/',views.session_get),
+    url(r'^mine/$',MyView.as_view(),name = 'my-view'),
 ]
--- django/pytest/test6/booktest/views.py
+++ django/pytest/test6/booktest/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
+
+class MyView(View):
+    def get(self,request,*args,**kwargs):
+        return HttpResponse('Hello,world!')
 
 # Create your views here.
 # 创建session_set和session_get视图如下（测试redis存储session）
-def session_set(request):
-    request.session['name'] = 'zax'
-    return HttpResponse('ok')
+# def session_set(request):
+#     request.session['name'] = 'zax'
+#     return HttpResponse('ok')
 
-def session_get(request):
-    name = request.session['name']
-    return HttpResponse(name)
+# def session_get(request):
+#     name = request.session['name']
+#     return HttpResponse(name)
+
+
 
