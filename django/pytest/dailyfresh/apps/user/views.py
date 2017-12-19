76af76f8a3d961acc4251855069c68db270c2c5b
kk <kk@kk.rhel.cc>
2017-12-19 17:59:16 +0800

    修改邮箱校验正则Bug

--- django/pytest/dailyfresh/apps/user/views.py
+++ django/pytest/dailyfresh/apps/user/views.py
@@ -28,11 +28,11 @@ class RegisterView(View):
         # 校验是否同意协议
         if allow !='on':
             return render(request,'register.html',{'errmsg':'请勾选协议'})
-
+        print("4444444444444")
         # 校验邮箱是否合法
-        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}{1,2})$',email):
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$',email):
             return render(request,'register.html',{'errmsg':'邮箱不合法'})
-        
+        print("222222222222")
         #校验用户名是否存在
         try:
             user = User.objects.get(username=username)
@@ -42,11 +42,12 @@ class RegisterView(View):
         if user:
             # 用户名已存在
             return render(request,'register.html',{'errmsg':'用户名已存在，请重新输入'})
-        
+        print("33333333333333")
         # 业务处理：用户注册
         user = User.objects.create_user(username,email,password)
         user.is_active = 0
         user.save()
+        print("111111111111111111", user)
 
         # 返回应答:跳转到首页
         return redirect(reverse('goods:index'))
