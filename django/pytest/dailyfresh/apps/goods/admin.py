e2c772f180b222a34ff5fbfd61996d1560750f43
kk <kk@kk.rhel.cc>
2017-12-25 15:24:39 +0800

    静态页面内容变更，静态首页重新生成

--- django/pytest/dailyfresh/apps/goods/admin.py
+++ django/pytest/dailyfresh/apps/goods/admin.py
@@ -9,11 +9,29 @@ class GoodsSpuAdmin(admin.ModelAdmin):
 class GoodsBannerAdmin(admin.ModelAdmin):
     list_display = ['id','sku']
 
+class IndexPromotionBannerAdmin(admin.ModelAdmin):
+    '''新增或者更新数据时被调用'''
+    def save_model(self, request, obj, form, change):
+        # 调用父类的方法，完成新增或者更新的操作
+        super().save_model(request, obj, form, change)
+        # 附加操作，发出generate_static_index_html任务
+        from celery_tasks.sendmail_task import generate_static_index_html
+        generate_static_index_html.delay()
+        print('send generate_static_index_html ok')
+
+    def delete_model(self, request, obj):
+        # 调用父类的方法，完成删除的操作
+        super().delete_model(request,obj)
+        # 附加操作，发出generate_static_index_html任务
+        from celery_tasks.sendmail_task import generate_static_index_html
+        generate_static_index_html.delay()
+
+
 admin.site.register(Goods,GoodsSpuAdmin) #商品SPU df_goods
 admin.site.register(GoodsType) #商品分类df_goods_type
 admin.site.register(GoodsSKU) #商品SKU df_goods_sku
 admin.site.register(IndexGoodsBanner,GoodsBannerAdmin) #商品轮播图df_index_banner
-admin.site.register(IndexPromotionBanner) #促销商品信息df_index_promotion
+admin.site.register(IndexPromotionBanner,IndexPromotionBannerAdmin) #促销商品信息df_index_promotion
 admin.site.register(IndexTypeGoodsBanner) #首页分类商品展示信息df_index_type_goods
 
 
