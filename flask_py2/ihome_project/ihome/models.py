8d09d9d0b6d6a1a9b8755487f926ac6fafd761fa
kk <kk@kk.rhel.cc>
2018-01-14 23:37:14 +0800

    user额外增加password属性实现密码加密

--- flask_py2/ihome_project/ihome/models.py
+++ flask_py2/ihome_project/ihome/models.py
@@ -15,7 +15,7 @@ class BaseModel(object):
 class User(BaseModel, db.Model):
     """用户"""
 
-    __tablename__ = "ih_uspasser_profile"
+    __tablename__ = "ih_user_profile"
 
     id = db.Column(db.Integer, primary_key=True)  # 用户编号
     name = db.Column(db.String(32), unique=True, nullable=False)  # 用户暱称
